6f5672a2e90ca83ded22d99d9c5ae8844366a212
Hervé Frezza-Buet <Herve.Frezza-Buet@centralesupelec.fr>
2022-11-27 16:04:46 +0100

    updt

--- librarie/pypaie/ligne.py
+++ librarie/pypaie/ligne.py
@@ -1,3 +1,5 @@
+import xlsxwriter
+
 class Ligne:
     def __init__(self, label):
         self.label = label
@@ -19,3 +21,21 @@ class Ligne:
         Renvoie le contenu de la colonne "pour information". None si inapproprié.
         """
         return None
+
+    def montants(self):
+        return self._brut(), self._cotisation_salariale(), self._cotisation_employeur()
+
+    def to_excel(self, worksheet, l,
+                 label_fmt, euro_fmt
+                 col_revenu, col_cot_sal, col_cot_pat):
+        
+        worksheet.write(l, col_label, self.label, label_fmt)
+        b, s, p = self.montants()
+        if b != None:
+            worksheet.write(l, col_revenu, b, euro_fmt)
+        if s != None:
+            worksheet.write(l, col_cot_sal, s, euro_fmt)
+        if p != None:
+            worksheet.write(l, col_cot_pst, p, euro_fmt)
+        
+    
--- librarie/pypaie/revenus.py
+++ librarie/pypaie/revenus.py
@@ -2,14 +2,25 @@
 from . import regles
 from . import ligne
 
-class Revenu:
-    def __init__(self, montant):
+class Revenu(ligne.Ligne):
+    def __init__(self, label, montant):
+        super().__init__(label)
         self.montant = montant
 
 
 class TraitementBrut(Revenu):
+    def __init__(self, montant):
+        super().__init__('Traitement brut', montant)
+        
+    def _brut(self):
+        return self.montant
+    
+class TraitementIndiciaireBrut(TraitementBrut):
     def __init__(self, indice):
         super().__init__(indice * regles.valeur_point_indice)
         self.indice = indice
+
+
+    
 
         
