4b8e141b6931c4d65f5bba06a67a68b4939b5107
Hervé Frezza-Buet <Herve.Frezza-Buet@centralesupelec.fr>
2022-11-29 18:28:38 +0100

    updt

--- exemples/documentation-elements.py
+++ exemples/documentation-elements.py
@@ -15,6 +15,7 @@ bulletin_paie += pp.revenus.TraitementBrut(1000)                    # Traitement
 bulletin_paie += pp.revenus.TraitementIndiciaireBrut(500)           # Traitement brut d'indice 500.     
 bulletin_paie += pp.revenus.PrimePublic(200)                        # Prime d'un fonctionnaire de 200 euros.
 bulletin_paie += pp.revenus.PrimePrive(200)                         # Prime d'un contractuel / salarié du privé de 200 euros.
+bulletin_paie += pp.revenus.IndemniteFonctions(200)                 # Prime d'un fonctionnaire de 200 euros.
 bulletin_paie += pp.revenus.TransfertPrimesPoints(200)              # Transfert primes/points (fonctionnaires) de 200 euros.
 bulletin_paie += pp.revenus.IndemniteResidence(20)                  # Indemnité de résidence de 20 euros.
 bulletin_paie += pp.revenus.SupplementFamilial(20)                  # Supplément familial de traitement de 20 euros.               
